f8d850ddf36c84e2723b248be19805c75cc73937
신석윤 <shinsy9666@gmail.com>
2026-07-30 14:08:03 +0900

    Polish combat, pause menu, and augment icons

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'su-op-adventure-v7';
+const CACHE_NAME = 'su-op-adventure-v8';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
   './assets/pwa-icon-192.png',
   './assets/pwa-icon-512.png',
+  './assets/augment-icons-chroma.png',
   './assets/augment-speed-boot-chroma.png',
   './assets/sprite-player.png',
   './assets/sprite-bat.png',
